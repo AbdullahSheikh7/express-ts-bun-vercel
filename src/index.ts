84e48c48fc12b0edb5b645d31dc16d5dcf7528fe
Abdullah <abdullahwindows10user@gmail.com>
2026-08-06 02:01:01 +0500

    Removed extensions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import { port } from "./config/env.js";
+import { port } from "./config/env";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
